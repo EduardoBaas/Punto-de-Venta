c5a2cb53cc445304b16afb0539c06ce7e9181452
Eduardo Baas Kauil <41127468+EduardoBaas@users.noreply.github.com>
2018-07-11 19:53:30 -0500

    creacion del formulario de login

---
+++ PuntoDeVenta/frmLogin.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: pumas
+ * Date: 11/07/2018
+ * Time: 07:52 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PuntoDeVenta
+{
+	/// <summary>
+	/// Description of frmLogin.
+	/// </summary>
+	public partial class frmLogin : Form
+	{
+		public frmLogin()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
