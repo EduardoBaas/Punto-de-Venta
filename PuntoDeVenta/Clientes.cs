eb8f0f598e8a69dc5384a122494c83dfaff832ce
JairDesaillyPachecoPerez <desaipacheco@gmail.com>
2018-07-25 09:03:58 -0500

    avance reaprado
    olvide cambiar algunas cosas

---
+++ PuntoDeVenta/Clientes.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jakepp09
+ * Date: 25/07/2018
+ * Time: 09:01 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PuntoDeVenta
+{
+	/// <summary>
+	/// Description of Clientes.
+	/// </summary>
+	public class Clientes
+	{
+		public Clientes()
+		{
+		}
+	}
+}
