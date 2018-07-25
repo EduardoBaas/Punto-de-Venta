744d26e7c8a61f07be6cf1f7572ec0007c6becd6
JairDesaillyPachecoPerez <desaipacheco@gmail.com>
2018-07-25 08:00:20 -0500

    avance martes 24

--- PuntoDeVenta/frmClientes.Designer.cs
+++ PuntoDeVenta/frmClientes.Designer.cs
@@ -14,6 +14,22 @@ namespace PuntoDeVenta
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.PictureBox pictureBox8;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox txtBusc;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.DataGridView dgvProductos;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtExistencia;
+		private System.Windows.Forms.Button btnGuardar;
+		private System.Windows.Forms.Button btnEditar;
+		private System.Windows.Forms.Button btnEliminar;
+		private System.Windows.Forms.TextBox txtMinimo;
+		private System.Windows.Forms.TextBox txtMaximo;
+		private System.Windows.Forms.TextBox txtCosto;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -37,17 +53,271 @@ namespace PuntoDeVenta
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClientes));
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.pictureBox8 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.txtExistencia = new System.Windows.Forms.TextBox();
+			this.btnGuardar = new System.Windows.Forms.Button();
+			this.btnEditar = new System.Windows.Forms.Button();
+			this.btnEliminar = new System.Windows.Forms.Button();
+			this.txtMinimo = new System.Windows.Forms.TextBox();
+			this.txtMaximo = new System.Windows.Forms.TextBox();
+			this.txtCosto = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtBusc = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.dgvProductos = new System.Windows.Forms.DataGridView();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.panel2.Controls.Add(this.label10);
+			this.panel2.Controls.Add(this.label9);
+			this.panel2.Controls.Add(this.pictureBox8);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(371, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(983, 50);
+			this.panel2.TabIndex = 30;
+			// 
+			// label10
+			// 
+			this.label10.Font = new System.Drawing.Font("Candara", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label10.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.label10.Location = new System.Drawing.Point(216, 6);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(124, 40);
+			this.label10.TabIndex = 25;
+			this.label10.Text = "Systems";
+			// 
+			// label9
+			// 
+			this.label9.Font = new System.Drawing.Font("Candara", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.ForeColor = System.Drawing.Color.MidnightBlue;
+			this.label9.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.label9.Location = new System.Drawing.Point(96, 6);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(124, 40);
+			this.label9.TabIndex = 24;
+			this.label9.Text = "Absolute";
+			// 
+			// pictureBox8
+			// 
+			this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+			this.pictureBox8.Location = new System.Drawing.Point(6, 0);
+			this.pictureBox8.Name = "pictureBox8";
+			this.pictureBox8.Size = new System.Drawing.Size(100, 49);
+			this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox8.TabIndex = 23;
+			this.pictureBox8.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.panel1.Controls.Add(this.txtExistencia);
+			this.panel1.Controls.Add(this.btnGuardar);
+			this.panel1.Controls.Add(this.btnEditar);
+			this.panel1.Controls.Add(this.btnEliminar);
+			this.panel1.Controls.Add(this.txtMinimo);
+			this.panel1.Controls.Add(this.txtMaximo);
+			this.panel1.Controls.Add(this.txtCosto);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(371, 541);
+			this.panel1.TabIndex = 29;
+			// 
+			// txtExistencia
+			// 
+			this.txtExistencia.BackColor = System.Drawing.Color.LightSteelBlue;
+			this.txtExistencia.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtExistencia.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtExistencia.Location = new System.Drawing.Point(37, 299);
+			this.txtExistencia.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtExistencia.Name = "txtExistencia";
+			this.txtExistencia.Size = new System.Drawing.Size(292, 26);
+			this.txtExistencia.TabIndex = 39;
+			// 
+			// btnGuardar
+			// 
+			this.btnGuardar.BackColor = System.Drawing.Color.White;
+			this.btnGuardar.ForeColor = System.Drawing.Color.MediumBlue;
+			this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+			this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnGuardar.Location = new System.Drawing.Point(24, 378);
+			this.btnGuardar.Name = "btnGuardar";
+			this.btnGuardar.Size = new System.Drawing.Size(96, 37);
+			this.btnGuardar.TabIndex = 33;
+			this.btnGuardar.Text = "Guardar";
+			this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.btnGuardar.UseVisualStyleBackColor = false;
+			// 
+			// btnEditar
+			// 
+			this.btnEditar.BackColor = System.Drawing.Color.White;
+			this.btnEditar.ForeColor = System.Drawing.Color.MediumBlue;
+			this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+			this.btnEditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnEditar.Location = new System.Drawing.Point(137, 378);
+			this.btnEditar.Name = "btnEditar";
+			this.btnEditar.Size = new System.Drawing.Size(96, 37);
+			this.btnEditar.TabIndex = 32;
+			this.btnEditar.Text = "Editar";
+			this.btnEditar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.btnEditar.UseVisualStyleBackColor = false;
+			// 
+			// btnEliminar
+			// 
+			this.btnEliminar.BackColor = System.Drawing.Color.White;
+			this.btnEliminar.ForeColor = System.Drawing.Color.Crimson;
+			this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+			this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btnEliminar.Location = new System.Drawing.Point(250, 378);
+			this.btnEliminar.Name = "btnEliminar";
+			this.btnEliminar.Size = new System.Drawing.Size(96, 37);
+			this.btnEliminar.TabIndex = 31;
+			this.btnEliminar.Text = "Eliminar";
+			this.btnEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.btnEliminar.UseVisualStyleBackColor = false;
+			// 
+			// txtMinimo
+			// 
+			this.txtMinimo.BackColor = System.Drawing.Color.LightSteelBlue;
+			this.txtMinimo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtMinimo.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtMinimo.Location = new System.Drawing.Point(37, 170);
+			this.txtMinimo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtMinimo.Name = "txtMinimo";
+			this.txtMinimo.Size = new System.Drawing.Size(292, 26);
+			this.txtMinimo.TabIndex = 30;
+			// 
+			// txtMaximo
+			// 
+			this.txtMaximo.BackColor = System.Drawing.Color.LightSteelBlue;
+			this.txtMaximo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtMaximo.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtMaximo.Location = new System.Drawing.Point(37, 236);
+			this.txtMaximo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtMaximo.Name = "txtMaximo";
+			this.txtMaximo.Size = new System.Drawing.Size(292, 26);
+			this.txtMaximo.TabIndex = 29;
+			// 
+			// txtCosto
+			// 
+			this.txtCosto.BackColor = System.Drawing.Color.LightSteelBlue;
+			this.txtCosto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtCosto.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCosto.Location = new System.Drawing.Point(37, 100);
+			this.txtCosto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtCosto.Name = "txtCosto";
+			this.txtCosto.Size = new System.Drawing.Size(292, 26);
+			this.txtCosto.TabIndex = 28;
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(37, 16);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(292, 34);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Agregar o editar clientes:";
+			// 
+			// txtBusc
+			// 
+			this.txtBusc.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.txtBusc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtBusc.Location = new System.Drawing.Point(459, 56);
+			this.txtBusc.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.txtBusc.Name = "txtBusc";
+			this.txtBusc.Size = new System.Drawing.Size(551, 20);
+			this.txtBusc.TabIndex = 33;
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.ForeColor = System.Drawing.Color.Black;
+			this.label8.Location = new System.Drawing.Point(387, 59);
+			this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(133, 25);
+			this.label8.TabIndex = 32;
+			this.label8.Text = "Buscar";
+			// 
+			// dgvProductos
+			// 
+			this.dgvProductos.AllowUserToAddRows = false;
+			this.dgvProductos.AllowUserToDeleteRows = false;
+			this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvProductos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+			this.dgvProductos.BackgroundColor = System.Drawing.Color.Lavender;
+			this.dgvProductos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.dgvProductos.ColumnHeadersHeight = 35;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.Color.DodgerBlue;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.dgvProductos.DefaultCellStyle = dataGridViewCellStyle1;
+			this.dgvProductos.GridColor = System.Drawing.Color.Lavender;
+			this.dgvProductos.Location = new System.Drawing.Point(387, 87);
+			this.dgvProductos.Name = "dgvProductos";
+			this.dgvProductos.ReadOnly = true;
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.Color.Transparent;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dgvProductos.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+			this.dgvProductos.RowHeadersVisible = false;
+			this.dgvProductos.RowHeadersWidth = 120;
+			dataGridViewCellStyle3.BackColor = System.Drawing.Color.Lavender;
+			dataGridViewCellStyle3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+			dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ActiveCaption;
+			dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+			this.dgvProductos.RowsDefaultCellStyle = dataGridViewCellStyle3;
+			this.dgvProductos.Size = new System.Drawing.Size(925, 423);
+			this.dgvProductos.TabIndex = 31;
 			// 
 			// frmClientes
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.ClientSize = new System.Drawing.Size(1354, 541);
+			this.Controls.Add(this.txtBusc);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.dgvProductos);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "frmClientes";
 			this.Text = "frmClientes";
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
